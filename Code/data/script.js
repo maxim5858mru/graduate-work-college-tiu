e82afa22c752a62a35f64fae4f0177533a8911b2
Максим Кулаков <user@localhost.localdomain>
2020-02-19 22:22:49 +0500

    Altium, test EEPROM, text and etc.

---
+++ Code/data/script.js
@@ -0,0 +1,15 @@
+﻿function showMenu() {
+    if (navMenu.style.display == "block") {
+        navMenu.style.display = "none";
+        search.style.display = "none";
+        navButton.innerHTML = navButton.innerHTML.slice(0, navButton.innerHTML.indexOf("Меню")) + active.innerHTML.slice(active.innerHTML.indexOf("</i>")+4, active.innerHTML.indexOf("</a>"));
+    }
+    else {
+        navMenu.style.display = "block";
+        search.style.display = "block";
+        navButton.innerHTML = navButton.innerHTML.slice(0, navButton.innerHTML.indexOf("</button")+9)+"Меню";
+    }
+}
+window.onload = function ready(){
+    navButton.innerHTML = navButton.innerHTML.slice(0, navButton.innerHTML.indexOf("Меню")) + active.innerHTML.slice(active.innerHTML.indexOf("</i>")+4, active.innerHTML.indexOf("</a>"));
+};
